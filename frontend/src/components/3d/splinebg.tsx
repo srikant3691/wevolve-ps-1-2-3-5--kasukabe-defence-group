8a314b076bfaf507647c7b9bd4d5b8f2a934c1bd
priyanshunitr <sahupriyanshu2006@gmail.com>
2026-01-18 05:33:10 +0530

    3d asset

---
+++ frontend/src/components/3d/splinebg.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Spline from "@splinetool/react-spline";
+
+export default function SplineBg() {
+  return (
+    <div>
+      <Spline scene="https://prod.spline.design/eptFcGbRN8-SmKyB/scene.splinecode" />
+    </div>
+  );
+}
